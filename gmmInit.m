f9265acf73fd0886eefca01eb56c12d3d878787c
pkukulak <philip.kukulak@mail.utoronto.ca>
2016-03-29 14:07:09 -0400

    gmms are initialized properly from the loaded data

--- gmmInit.m
+++ gmmInit.m
@@ -1,13 +1,39 @@
-function [ mix, mu, sig ] = gmmInit(M, d)
+function init_res = gmmInit(M, d, spkrDir)
 % Initialize the parameters for a Gaussian mixture model.
+%
 % INPUT:
-%   M   - the number of Gaussian models to be mixed.
-%   d   - the dimension/feature count of input data.
+%   M       - the number of Gaussian models to be mixed.
+%   d       - the dimension/feature count of input data.
+%   spkrDir - the directory where data for this speaker is stored.
 %
 % OUTPUT:
-%   mix - The mixing coefficients of each model.
-%   mu  - The mean vectors of each model.
-%   sig - The variance vectors of each model.
+%   w       - The mixing coefficients of each model.
+%   mu      - The mean vectors of each model.
+%   cov     - The variance vectors of each model.
+%   X       - the MFCC data matrix for the speaker.
 
+% Construct this speaker's MFCC matrix, X.
+% X is of dimension T x d, where T = t_1 + .. + t_9
+% That is, we vertically stack all MFCC frames of each
+% utterance for this speaker.
+X = zeros(0, d);
+spkrDD = dir([spkrDir, filesep, '*.mfcc']);
+
+for iFile=1:length(spkrDD)
+    
+    speakerFn = strcat(spkrDir, '/', spkrDD(iFile).name);
+    X = [X; textread(speakerFn)];
 end
 
+% Covariance matrices are diagonal, so we initialize
+% a d x M matrix, where each column i represents the diagonal
+% of a d x d covariance matrix for component i.
+
+% We return a struct with the initialized parameters and data
+% as fields.
+init_res.w = ones(M, 1) / M;
+init_res.mu = X(ceil(rand * size(X, 1)), :);
+init_res.sig = ones(d, M);
+init_res.X = X;
+
+end
--- gmmTrain.m
+++ gmmTrain.m
@@ -1,21 +1,33 @@
-function GMM = gmmTrain(dataDir, fn_GMM)
+function GMM = gmmTrain(dataDir, fn_GMM, M, max_iter)
 % Train a Gaussian mixture model for every speaker in
 % the data set and save their model.
 %
 % INPUT:
-%   dataDir - the top-level directory from which to train
-%             each model, i.e. '/u/cs401/speechdata/Training/'
-%   fn_GMM  - the part of the filename of each saved model
-%             that is common to all models. for example, a
-%             speaker whose data is stored in the subdirectory
-%             'FEDC0' would produce the model saved in
-%             [fn_gmm] + '_FEDC0.mat'
+%   dataDir  - the top-level directory from which to train
+%              each model, i.e. '/u/cs401/speechdata/Training/'
+%   fn_GMM   - the part of the filename of each saved model
+%              that is common to all models. for example, a
+%              speaker whose data is stored in the subdirectory
+%              'FECD0' would produce the model saved in
+%              [fn_gmm] + '_FECD0.mat'
+%   M        - the number of gaussians mixed into each mixture.
+%   max_iter - the maximum iterations to train each mixture.
 %
 % OUTPUT:
-%   GMM     - a struct containing all GMMs. that is, a GMM for
-%             each speaker in the training set.
+%   GMM      - a struct containing all GMMs. that is, a GMM for
+%              each speaker in the training set.
 
+% All MFCC vectors have 14 features and then a log summary.
+d = 14 + 1;
 
+GMM = struct();
 
+topDD = dir([dataDir, filesep, '*']);
+for iDir=3:length(topDD)
+    speakerDir = strcat(dataDir, topDD(iDir).name);
+    
+    % This speaker's GMM.
+    GMM.(topDD(iDir).name) = gmmInit(M, d, speakerDir);
+    
 end
 
