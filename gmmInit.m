00482ed49f9fa753ff6185e3cf912ce52037426e
pkukulak <philip.kukulak@mail.utoronto.ca>
2016-03-29 12:19:23 -0400

    hungry skelton of gmmInit function

---
+++ gmmInit.m
@@ -0,0 +1,13 @@
+function [ mix, mu, sig ] = gmmInit(M, d)
+% Initialize the parameters for a Gaussian mixture model.
+% INPUT:
+%   M   - the number of Gaussian models to be mixed.
+%   d   - the dimension/feature count of input data.
+%
+% OUTPUT:
+%   mix - The mixing coefficients of each model.
+%   mu  - The mean vectors of each model.
+%   sig - The variance vectors of each model.
+
+end
+
