b31017f383604910e52635712f939d01beb8daf6
pkukulak <philip.kukulak@mail.utoronto.ca>
2016-04-05 19:45:54 -0400

    GMM training for part 2.1

--- gmmTrain.m
+++ gmmTrain.m
@@ -27,22 +27,52 @@ GMM = struct();
 
 topDD = dir([dataDir, filesep, '*']);
 for iDir=3:length(topDD)
+    spkr_name = topDD(iDir).name;
     speakerDir = strcat(dataDir, topDD(iDir).name);
+    
     % This speaker's GMM.
-    GMM.(topDD(iDir).name) = gmmInit(M, d, speakerDir);
+    GMM.(spkr_name) = gmmInit(M, d, speakerDir);
+    curr_X = GMM.(spkr_name).X;
+    T = size(curr_X, 1);
     
     i = 0;
-    prev_ll = -Inf;
+    b = zeros(T, M);
+    LL = zeros(T, M);
+    prev_LL = zeros(T, M) * -Inf;
     diff = Inf;
-    T = size(GMM.(topDD(iDir).name.X), 1);
-    
-    %while (i <= max_iter && diff >= eps)
-    %    for m=1:M
-    %        meansRep = repmat(gmm.(topDD(iDir).name).mu(m,:), T, 1);
-    %        covRep = repmat(gmm.(topDD(iDir).name).sig(m, :), T, 1);
-    %        minsMeansSquare = (gmm.(topDD(iDir).name).X - meansRep).^2;
-    %        normalized = 
-    %    end
-    %end
+
+    disp(spkr_name);
+    while (i < max_iter && diff >= eps)
+        curr_mu = GMM.(spkr_name).mu;
+        curr_sig = GMM.(spkr_name).sig;
+        curr_w = GMM.(spkr_name).w;
+       
+        % First, calculate all b values.
+        for i=1:M
+            mu_m = curr_mu(i, :);
+            sig_m = curr_sig(i, :);
+            log_b_m = -(sum(bsxfun(@minus, curr_X, mu_m).^2, 2) ...
+                      / 2 * sum(sig_m)) - (d / 2) * log(2 * pi) ...
+                      - (.5 * log(prod(sig_m)));
+            b(:, i) = log_b_m;
+        end
+        
+        % Then, for every model, calculate likelihood.
+        denom = bsxfun(@times, b, curr_w.');
+        numer = sum(denom, 2);
+        LL = bsxfun(@rdivide, denom, numer);
+        
+        % Finally, update parameters.
+        GMM.(spkr_name).w =  (sum(LL, 1) / T).';
+        GMM.(spkr_name).mu = bsxfun(@rdivide, LL.' * curr_X, ...
+                                    sum(LL, 1).');
+        GMM.(spkr_name).sig = bsxfun(@rdivide, LL.' * (curr_X.^2), ...
+                             sum(LL, 1).') - (GMM.(spkr_name).mu).^2; 
+                         
+        % Update convergence criterion.
+        diff = norm(LL - prev_LL);
+        prev_LL = LL;
+        i = i + 1;
+    end
 end
 
